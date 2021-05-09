98942f3ffa8ede976d0c0e7b497167384c2aa6d3
Максим Сергеев <msergeev@softrust.ru>
2021-05-09 14:59:22 +0300

    Добавлена логика работы обратного отсчета таймера. Поведение полей на форме и мелкие правки

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,81 +20,112 @@ namespace Sleeptimer
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
-        private int second, minute, hour = 0;
-        private string secondText ,minuteText, hourText;
+    { 
         DispatcherTimer timer = new DispatcherTimer();
+        DateTime time, timeSlider;
+        private int secondSlider = 0;
+        private CurrentStatus currentStatus = CurrentStatus.Wait;
+        enum CurrentStatus
+        {
+            Start,
+            Stop,
+            Pause,
+            Wait
+        }
         public MainWindow()
         {
             InitializeComponent();
+            RefreshEnableControls();
         }
-        private void StartStop_Click(object sender, RoutedEventArgs e)
+        private void Start_Click(object sender, RoutedEventArgs e)
         {
-            TimeFirst.IsEnabled = false;
-            if (StartStop.Content == "Стоп")
-            {
-                StartStop.Content = "Старт";
-                timer.Stop();
-                return;
-            }
-         
+            TimerStart();
+        }
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            TimerStop();
+        }
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+            TimerPause();
+        }
+        
+        private void TimerStart()
+        {
+            timer = new DispatcherTimer();
+
+            if (currentStatus != CurrentStatus.Pause) 
+                ResetTime(); 
+
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += timer_Tick;
             timer.Start();
-            StartStop.Content = "Стоп";
+            currentStatus = CurrentStatus.Start;
+            RefreshEnableControls();
+
+        }
+
+        private void ResetTime()
+        {
+            TimeFirst.Text = TimeSecond.Text;
+            time = DateTime.Parse(TimeFirst.Text);
+        }
+
+        private void TimerStop()
+        {
+            ResetTime();
+            timer.Stop();
+            currentStatus = CurrentStatus.Stop;
+            RefreshEnableControls();
+        }
+
+        private void TimerPause()
+        {
+            timer.Stop();
+            currentStatus = CurrentStatus.Pause;
+            RefreshEnableControls();
         }
 
         private void timer_Tick(object sender, EventArgs e)
+        { 
+            time = time.AddSeconds(-1);  
+            TimeFirst.Text = time.ToString("HH:mm:ss"); 
+        }
+
+        private void RefreshEnableControls()
         {
-            second++;
-
-            if (second > 59)
+            switch (currentStatus)
             {
-                minute++;
-                second = 0;
+                case CurrentStatus.Start: 
+                    Start.IsEnabled = PanelMode.IsEnabled = PanelEditTime.IsEnabled = false;
+                    Stop.IsEnabled = Pause.IsEnabled = true; 
+                    break;
+                case CurrentStatus.Stop: 
+                    Stop.IsEnabled = Pause.IsEnabled = false; 
+                    Start.IsEnabled = PanelMode.IsEnabled = PanelEditTime.IsEnabled = true;
+                    break;
+                case CurrentStatus.Pause: 
+                    Pause.IsEnabled = PanelEditTime.IsEnabled = false;
+                    Start.IsEnabled = Stop.IsEnabled = PanelMode.IsEnabled = true; 
+                    break;
+                case CurrentStatus.Wait:
+                    Stop.IsEnabled = Pause.IsEnabled = false;
+                    Start.IsEnabled = PanelMode.IsEnabled = PanelEditTime.IsEnabled = true; 
+                    break;
             }
+        }
 
-            if (minute > 59)
-            {
-                hour++;
-                minute = second = 0;
-            }
+        private void EndTimer()
+        {
 
-            if (hour > 59)
-            { 
-                minute = second = hour = 0;
-            }
+        }
 
-            if (second < 10)
-            {
-                secondText = $"0{second}";
-            }
-            else
-            {
-                secondText = second.ToString();
-            }
-
-            if (minute < 10)
-            {
-                minuteText = $"0{minute}";
-            }
-            else
-            {
-                minuteText = minute.ToString();
-            }
-
-            if (hour < 10)
-            {
-                hourText = $"0{hour}";
-            }
-            else
-            {
-                hourText = hour.ToString();
-            }
-            TimeFirst.Text = $"{hourText}:{minuteText}:{secondText}";
-
-
-
+        private void SliderTime_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            secondSlider = Convert.ToInt32(SliderTime.Value) * 600;
+            timeSlider = DateTime.ParseExact("00:00:00", "HH:mm:ss", null);
+            timeSlider = timeSlider.AddSeconds(secondSlider);
+            TimeSecond.Text = timeSlider.ToString("HH:mm:ss"); 
         }
     }
 }
